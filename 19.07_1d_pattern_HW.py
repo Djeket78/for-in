77b129366cce8546911beddd374c856e55d2d5b3
Evghenii R <108763119+Djeket78@users.noreply.github.com>
2022-07-20 22:31:36 +0300

    work done

---
+++ 19.07_1d_pattern_HW.py
@@ -0,0 +1,12 @@
+#     * * * # # * * * # # 
+
+print("\n")
+
+for x in range(1,21):
+    if x % 4 == 0:
+        print("# ",end="")
+        print("# ",end="")
+    else:
+        print("* ",end="")
+
+print("\n")
